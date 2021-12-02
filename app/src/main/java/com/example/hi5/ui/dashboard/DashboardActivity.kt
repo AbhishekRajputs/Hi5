59bfc13ea3aa81c9ea9709727161dbad40e02f1e
MIS <abhishek@vidyakul.com>
2021-12-02 23:19:42 +0530

    working on dashboard

--- app/src/main/java/com/example/hi5/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/example/hi5/ui/dashboard/DashboardActivity.kt
@@ -1,8 +1,10 @@
 package com.example.hi5.ui.dashboard
 
 import android.os.Bundle
+import com.example.hi5.R
 import com.example.hi5.base.BaseActivity
 import com.example.hi5.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DashboardActivity : BaseActivity() {
 
@@ -11,5 +13,29 @@ class DashboardActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.bottomNavigation.inflateMenu(R.menu.bottom_navigation_menu)
+        binding.bottomNavigation.setOnItemSelectedListener(bottomNavigationView)
+
+    }
+
+    private val bottomNavigationView = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.home -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.offers -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.rewards -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.discover -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.wallets -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 }
