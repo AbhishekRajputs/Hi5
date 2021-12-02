447e68365335a19aa0f76f5809446fc69d1d031d
MIS <abhishek@vidyakul.com>
2021-12-02 23:25:36 +0530

    working on bottom sheet

--- app/src/main/java/com/example/hi5/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/example/hi5/ui/dashboard/DashboardActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import com.example.hi5.R
 import com.example.hi5.base.BaseActivity
 import com.example.hi5.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DashboardActivity : BaseActivity() {
 
@@ -14,28 +13,25 @@ class DashboardActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.bottomNavigation.inflateMenu(R.menu.bottom_navigation_menu)
-        binding.bottomNavigation.setOnItemSelectedListener(bottomNavigationView)
-
-    }
-
-    private val bottomNavigationView = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.home -> {
-                return@OnNavigationItemSelectedListener true
+        binding.bottomNavigation.setOnItemSelectedListener{
+            when (it.itemId) {
+                R.id.home -> {
+                    return@setOnItemSelectedListener  true
+                }
+                R.id.offers -> {
+                    return@setOnItemSelectedListener  true
+                }
+                R.id.rewards -> {
+                    return@setOnItemSelectedListener  true
+                }
+                R.id.discover -> {
+                    return@setOnItemSelectedListener  true
+                }
+                R.id.wallets -> {
+                    return@setOnItemSelectedListener  true
+                }
             }
-            R.id.offers -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.rewards -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.discover -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.wallets -> {
-                return@OnNavigationItemSelectedListener true
-            }
+            false
         }
-        false
     }
 }
