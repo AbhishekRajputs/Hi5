f406ce66e9ef896915a505f6d0d6b49f54222351
MIS <abhishek@vidyakul.com>
2021-12-02 22:43:01 +0530

    working on issues

--- app/src/main/java/com/example/hi5/base/BaseActivity.kt
+++ app/src/main/java/com/example/hi5/base/BaseActivity.kt
@@ -1,11 +1,12 @@
-package com.example.hi5
+package com.example.hi5.base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hi5.R
 
-class MainActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/example/hi5/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,16 @@
+package com.example.hi5.ui.dashboard
+
+
+import android.os.Bundle
+import com.example.hi5.base.BaseActivity
+import com.example.hi5.databinding.ActivityMainBinding
+
+class DashboardActivity : BaseActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
