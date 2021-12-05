6758b991615c68acd5baf9cc41b79baded64e148
Pardeep <tv_welcome>
2021-12-05 11:35:48 +0530

    added click

---
+++ app/src/main/java/com/example/hi5/ui/offerDeatil/OfferDetailActivity.kt
@@ -0,0 +1,20 @@
+package com.example.hi5.ui.offerDeatil
+
+import android.os.Bundle
+import com.example.hi5.base.BaseActivity
+import com.example.hi5.databinding.ActivityOfferDetailBinding
+
+/**
+ * @author Pardeep Singh
+ */
+class OfferDetailActivity : BaseActivity() {
+
+    private val binding by lazy { ActivityOfferDetailBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.imgBack.setOnClickListener { onBackPressed() }
+        binding.btnSave.setOnClickListener { onBackPressed() }
+    }
+}
